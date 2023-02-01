723a57d90bb0b06ba509e7ca810085dd1abc12a4
bloneeee <blone5128@gmial.com>
2023-02-01 12:04:33 +0630

    l27 reduce > 1 Feb 2023 - 12:04PM

--- l27nanofinance/app.js
+++ l27nanofinance/app.js
@@ -15,18 +15,18 @@ const getlistgroup = document.getElementById('list-group');
 
 // console.log(balance, moneydeb, moneycrd, getform, gettranstatuses, getamount, getdate, getremark, getlistgroup);
 
-const dummydatas = [
-    {id: 1, remark: 'Petty Cash', transtatus: "+", amount: 1000, date: "2023-01-20"},
-    {id: 2, remark: 'Pen', transtatus: "-", amount: -20, date: "2023-01-21"},
-    {id: 3, remark: 'Other Income', transtatus: "+", amount: 300, date: "2023-01-25"},
-    {id: 4, remark: 'Book', transtatus: "-", amount: -10, date: "2023-01-25"},
-    {id: 5, remark: 'Water', transtatus: "-", amount: -150, date: "2023-01-25"},
-    {id: 6, remark: 'Teamix', transtatus: "-", amount: -100, date: "2023-01-25"}
-];
+// const dummydatas = [
+//     {id: 1, remark: 'Petty Cash', transtatus: "+", amount: 1000, date: "2023-01-20"},
+//     {id: 2, remark: 'Pen', transtatus: "-", amount: -20, date: "2023-01-21"},
+//     {id: 3, remark: 'Other Income', transtatus: "+", amount: 300, date: "2023-01-25"},
+//     {id: 4, remark: 'Book', transtatus: "-", amount: -10, date: "2023-01-25"},
+//     {id: 5, remark: 'Water', transtatus: "-", amount: -150, date: "2023-01-25"},
+//     {id: 6, remark: 'Teamix', transtatus: "-", amount: -100, date: "2023-01-25"}
+// ];
 // console.log(dummydatas);
 
-const getlsdatas = JSON.parse(localStorage.getItem('transaction'));
-let gethistories = localStorage.getItem('transaction') !== null ? getlsdatas : [];
+const getlsdatas = JSON.parse(localStorage.getItem('transactions'));
+let gethistories = localStorage.getItem('transactions') !== null ? getlsdatas : [];
 // console.log(getlsdatas, gethistories);
 
 var sign = "-";
@@ -57,8 +57,12 @@ function init(){
     // Method 2
     // dummydatas.forEach(dummydata => addtoui(dummydata));
 
-    // Method 3
-    dummydatas.forEach(addtoui);
+    // Method 3 ***
+    // dummydatas.forEach(addtoui);
+
+    gethistories.forEach(addtoui);
+
+    totalvalue();
 }
 init();
 
@@ -75,9 +79,54 @@ function addtoui(transaction){
         ${transaction.remark} 
         <span>${transaction.transtatus}${Math.abs(transaction.amount)}</span> 
         <span>${transaction.date}</span>
-        <button type="button" class="delete-btn">&times;</button>`;
+        <button type="button" class="delete-btn" onclick="removetransation(${transaction.id})">&times;</button>`;
 
     getlistgroup.appendChild(newli);
+}
+
+function totalvalue(){
+    const amounts = gethistories.map(gethistory => gethistory.amount);
+    // console.log(amounts);
+
+    // Method 1
+    // const result = amounts.reduce(function(total, curvalue){
+    //     total += curvalue;
+    //     return total;
+    // }, 0).toFixed(2);
+
+    // Method 2
+    const totalresult = amounts.reduce((total, curvalue) => (total += curvalue), 0).toFixed(2);
+
+    const debitesult = amounts
+        .filter(amount => amount > 0)
+        .reduce((total, curvalue) => (total += curvalue), 0).toFixed(2);
+
+    const creditresult = (amounts
+        .filter(amount => amount < 0)
+        .reduce((total, curvalue) => (total += curvalue), 0) * -1).toFixed(2);
+
+    // console.log(totalresult, debitesult, creditresult);
+
+    balance.innerText = `${totalresult}`;
+    moneydeb.innerText = `${debitesult}`;
+    moneycrd.innerText = `${creditresult}`;
+}
+// totalvalue();
+
+function generateidx(){
+    return Math.floor(Math.random() * 100000);
+}
+
+// Update Local Storage
+function updatlocalstorage(){
+    localStorage.setItem('transactions', JSON.stringify(gethistories));
+}
+
+function removetransation(tranid){
+    gethistories = gethistories.filter(gethistory => gethistory.id !== tranid);
+
+    updatlocalstorage();
+    init();
 }
 
 function newtransition(e){
@@ -88,7 +137,7 @@ function newtransition(e){
         alert('Oh!!! Some Data are missing.');
     }else{
         const transaction = {
-            id: 1000,
+            id: generateidx(),
             remark: getremark.value,
             transtatus: sign,
             amount: sign === "-" ? Number(-getamount.value) : Number(getamount.value),
@@ -97,7 +146,16 @@ function newtransition(e){
         // console.log(transaction);
 
         gethistories.push(transaction);
+        updatlocalstorage();
+
+        totalvalue();
+
         addtoui(transaction);
+
+        getamount.value = '';
+        getdate.value = '';
+        getremark.value = '';
+        getamount.focus();
     }
 }
 getform.addEventListener('submit',  newtransition);
@@ -105,3 +163,42 @@ getform.addEventListener('submit',  newtransition);
 openbtn.addEventListener('click', function(){
     gethisbox.classList.toggle('show');
 });
+
+// reduce Mathod
+
+// var myarrs = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// array.reduce(function(total, curentValue, currentIndex, array){}, initialValue);
+
+// var result = myarrs.reduce(function(total, value, idx, arr){
+//     // console.log(total, value, idx, arr);
+//     /* 
+//         if initialValue = "" 
+//         total = 10 to undefined
+//         value = 20 to 100
+//         idx = 1 to 9
+//         arr = array
+//     */ 
+
+//     /* 
+//         if initialValue = 0 
+//         total = 0 to undefined
+//         value = 10 to 100
+//         idx = 0 to 9
+//         arr = array
+//     */
+   
+//     /* 
+//         if initialValue = 1
+//         total = 1 to undefined
+//         value = 10 to 100
+//         idx = 0 to 9
+//         arr = array
+//     */
+
+//     total += value;
+//     // console.log(total);
+
+//     return total;
+// });
+
+// console.log(result);
