139e5624c294de38cdf18da84304d2510067599d
bloneeee <blone5128@gmial.com>
2023-03-02 21:41:26 +0630

    l30 > 2 Mar 2023

---
+++ l30visacard/script.js
@@ -0,0 +1,18 @@
+// Get UI
+const inputs = document.querySelectorAll("input");
+var datas = [];
+
+inputs.forEach(function(input){
+    input.addEventListener("keyup", function(){
+        // console.log(this.value);
+        
+        datas.push({
+            digit: inputs[0].value,
+            name: inputs[1].value,
+            exp: inputs[2].value,
+            cvc: inputs[3].value
+        });
+
+        console.log(datas[datas.length - 1]);
+    })
+});
